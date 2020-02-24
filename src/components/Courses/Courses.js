c2c5fc76b2bbee0ab95a9f6ac550e242a11120fc
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-02-23 19:49:48 -0500

    media, moda y mediana

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -6,7 +6,46 @@ class Courses extends React.Component {
       this.handleClick = this.handleClick.bind(this);
     }
     handleClick(val) {
-      console.log('Se hizo click', val);
+      // console.log('Se hizo click', val);
+      let data = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+      
+      const sorted = data.sort((a, b) => {return a - b})
+
+      let mean = 0
+      let median = 0
+      let mode = 0
+
+      let mf = 1;
+      let m = 0;
+
+
+      const sizeData = data.length
+      const is_odd = sizeData%2 ? true: false       
+       for (let i in data) {
+         mean += data[i]
+
+         if(is_odd){
+          median = sorted[Math.floor(sizeData/2)]
+         }else {
+           median = (sorted[Math.floor(sizeData/2)] + sorted[Math.floor(sizeData/2) + 1])/ 2
+         }
+         for (let j in data)
+         {
+                 if (data[i] === data[j])
+                  m++;
+                 if (mf<m)
+                 {
+                   mf=m; 
+                   mode = data[i];
+                 }
+         }
+         m=0;
+
+       }
+       console.log("La media es "+ mean/sizeData)
+       console.log("La mediana es "+ median)
+       console.log("La moda es "+ mode)
+
     }
   render() {
     return (
