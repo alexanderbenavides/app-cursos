6ff19f0881e19ba3756b5f657aa2af2d8403a121
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-02-29 15:40:15 -0500

    armando las páginas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,38 @@
 import React from "react";
+import {
+  Route,
+  // NavLink,
+  // HashRouter,
+  BrowserRouter as Router,
+  Link,
+  Switch
+} from "react-router-dom";
 import "./App.scss";
 import CoursesList from "../CoursesList/CoursesList";
 import Header from "../Header/Header";
 import Clients from "../clientes/Cliente";
 import axios from "axios";
 import url from "../../api/urlRequest";
-
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import { DatePicker, Select } from "antd";
+import Admin from "../../pages/Admin";
+import Home from "../../pages/Home";
+const { Option } = Select;
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+function error404() {
+  return <h1>Página no encontrada</h1>;
+}
 
 class App extends React.Component {
   state = {
     coursesList: []
   };
+
+  constructor(props) {
+    super(props);
+    this.changeDate = this.changeDate.bind(this);
+  }
 
   componentDidMount() {
     axios.get(`${url}/courses`).then(res => {
@@ -19,7 +40,9 @@ class App extends React.Component {
       this.setState({ coursesList });
     });
   }
-
+  changeDate(e) {
+    console.log(e._d);
+  }
   searchPost(data) {
     const coursesList = [];
     if (data.ok === true) {
@@ -35,38 +58,54 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header searchPost={this.searchPost.bind(this)}></Header>
-        <HashRouter>
-                  
+        <DatePicker onChange={this.changeDate} />
+        <Admin></Admin>
+        <Home></Home>
+        <Router>
           <div>
                       
             <ul className="header">
                           
               <li>
-                <NavLink to="/clients">Clientes</NavLink>
+                <Link to="/clients">Clientes</Link>
               </li>
                           
               <li>
-                <NavLink to="/cursos">Cursos</NavLink>
-              </li>
-              <h1>dkkdkdkdkkdkd</h1>
+                <Link to="/">Cursos</Link>
+              </li>{" "}
                         
             </ul>
                       
             <div className="content">
-                          
-              <Route path="/clients" component={Clients} />
-              <Route
-                path="/cursos"
-                component={() => (
-                  <CoursesList coursesData={this.state.coursesList} />
-                )}
-              />
+              <Switch>
+                <Route exact path="/clients" component={Clients} />
+                <Route
+                  exact
+                  path="/"
+                  component={() => (
+                    <CoursesList coursesData={this.state.coursesList} />
+                  )}
+                />
+                <Route component={error404} />
+              </Switch>
                         
             </div>
                     
           </div>
-                
-        </HashRouter>
+        </Router>
+        <Select
+          defaultValue="lucy"
+          style={{ width: 120 }}
+          onChange={handleChange}
+        >
+          <Option value="jack">Jack</Option>
+          <Option value="lucy">Lucy</Option>
+          <Option value="disabled" disabled>
+            Disabled
+          </Option>
+          <Option value="Yiminghe">yiminghe</Option>
+        </Select>
+              
       </div>
     );
   }
---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <div className="Admin">
+        <h1>Este es el admin</h1>
+      </div>
+    );
+  }
+}
+
+export default Admin;
