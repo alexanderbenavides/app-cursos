7bd38073ff60e2134038a60d2c5421ff675097e0
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-03-04 06:26:58 -0500

    agregando modales

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Modal } from "antd";
 import { PlusCircleOutlined } from "@ant-design/icons";
 
 import "./Users.scss";
@@ -7,15 +8,58 @@ class Users extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      ModalText: "Content of the modal",
+      visible: false,
+      confirmLoading: false
+    };
   }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      ModalText: "The modal will be closed after two seconds",
+      confirmLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false
+      });
+    }, 2000);
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    });
+  };
   handleClick() {
     console.log("Hola mundo");
   }
+
   render() {
+    const { visible, confirmLoading, ModalText } = this.state;
     return (
       <div className="user__page">
-        <h1>Página de users</h1>
-        <PlusCircleOutlined onClick={this.handleClick} />
+        <PlusCircleOutlined
+          onClick={this.showModal}
+          style={{ fontSize: "20px" }}
+        />
+        <Modal
+          title="Crear usuario"
+          visible={visible}
+          onOk={this.handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <p>{ModalText}</p>
+        </Modal>
       </div>
     );
   }
