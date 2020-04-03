7e3bf349b2c49a3561f334fe10a4680f560a1f75
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-04-03 10:28:56 -0500

    ambiente de desarrollo

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,9 @@
-// let basePath = "";
+let basePath = "";
 const apiVersion = "v1";
-
-// const environment = process.env.NODE_ENV;
-// if (environment === "development") {
-// const basePath = "http://localhost:3977/api";
-const basePath = "https://quipuweb.herokuapp.com/api";
-// }
+const environment = process.env.NODE_ENV;
+if (environment === "development") {
+  basePath = "http://localhost:3977/api";
+} else {
+  basePath = "https://quipuweb.herokuapp.com/api";
+}
 export { basePath, apiVersion };
