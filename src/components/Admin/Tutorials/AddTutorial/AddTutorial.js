7ca878587a1210f5bebd8fbd57dca968cfb10bcd
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-04-04 17:28:47 -0500

    codesample edito

--- src/components/Admin/Tutorials/AddTutorial/AddTutorial.js
+++ src/components/Admin/Tutorials/AddTutorial/AddTutorial.js
@@ -134,11 +134,25 @@ class AddTutorial extends React.Component {
                       "advlist autolink lists link image charmap print preview anchor",
                       "searchreplace visualblocks code fullscreen",
                       "insertdatetime media table paste code help wordcount",
+                      "codesample",
+                    ],
+                    codesample_languages: [
+                      { text: "HTML/XML", value: "markup" },
+                      { text: "JavaScript", value: "javascript" },
+                      { text: "CSS", value: "css" },
+                      { text: "PHP", value: "php" },
+                      { text: "Ruby", value: "ruby" },
+                      { text: "Python", value: "python" },
+                      { text: "Java", value: "java" },
+                      { text: "C", value: "c" },
+                      { text: "C#", value: "csharp" },
+                      { text: "C++", value: "cpp" },
                     ],
                     toolbar:
                       "undo redo | formatselect | bold italic backcolor | \
                     alignleft aligncenter alignright alignjustify | \
-                    bullist numlist outdent indent | removeformat | help",
+                    bullist numlist outdent indent | removeformat | help | \
+                    codesample",
                   }}
                   onEditorChange={(e) => this.onChangeProperty(e, "content")}
                   onBlur={(e) => {
