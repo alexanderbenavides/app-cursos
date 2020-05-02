c8b6135b193e05f901048cb6309a4ee962721265
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-05-02 09:02:40 -0500

    hooks en lessons

--- src/pages/Home/Lessons/Lessons.js
+++ src/pages/Home/Lessons/Lessons.js
@@ -1,28 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, notification } from "antd";
 import { Helmet } from "react-helmet";
 import { getLessonsHomeByModuleApi } from "../../../api/lesson";
 import "../../../scss/_lessons.scss";
+function Lessons(props) {
+  const { course, module, moduleTitle } = props.match.params;
+  const [currentPage, setCurrentPage] = useState(0);
+  const [hideNextButton, setHideNextButton] = useState(false);
+  const [hidePreviusButton, setHidePreviusButton] = useState(true);
+  const [lessonData, setLessonData] = useState([]);
 
-class Lessons extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: this.props.match.params.course,
-      module: this.props.match.params.module,
-      lessonData: [],
-      moduleTitle: this.props.match.params.moduleTitle,
-      currentPage: 0,
-      hideNextButton: false,
-      hidePreviusButton: true,
-    };
-  }
-  componentDidMount() {
-    const { module } = this.state;
-    this.getLessonsByModule(module);
-  }
-  getLessonsByModule = (module) => {
+  useEffect(() => {
     getLessonsHomeByModuleApi(module)
       .then((response) => {
         if (response?.status !== 200) {
@@ -30,7 +19,7 @@ class Lessons extends React.Component {
             message: response.message,
           });
         } else {
-          this.setState({ lessonData: response.data.lessons, module });
+          setLessonData(response.data.lessons);
         }
       })
       .catch(() => {
@@ -39,123 +28,107 @@ class Lessons extends React.Component {
             "No se pudieron obtener los temas por un error del servidor. Por favor,inténtelo más tarde.",
         });
       });
-  };
-  handleNextLesson = () => {
-    const lessonsSize = this.state.lessonData.length;
-    const { currentPage } = this.state;
+  }, []);
+
+  const handleNextLesson = () => {
+    const lessonsSize = lessonData.length;
     if (currentPage < lessonsSize) {
-      this.setState({
-        hidePreviusButton: false,
-        currentPage: this.state.currentPage + 1,
-      });
+      setHidePreviusButton(false);
+      setCurrentPage(currentPage + 1);
 
       if (lessonsSize - currentPage === 2) {
-        this.setState({
-          hideNextButton: true,
-        });
+        setHideNextButton(true);
       }
     }
   };
-  handlePreviuosLesson = () => {
-    this.setState({
-      hideNextButton: false,
-      currentPage: this.state.currentPage - 1,
-    });
-    const { currentPage } = this.state;
+  const handlePreviuosLesson = () => {
+    setHideNextButton(false);
+    setCurrentPage(currentPage - 1);
     if (currentPage === 1) {
-      this.setState({
-        hidePreviusButton: true,
-      });
+      setHidePreviusButton(true);
     }
   };
-  render() {
-    const currentPage = this.state.currentPage;
-    let lessonBodyData = {},
-      currentClass = "";
 
-    const {
-      lessonData,
-      moduleTitle,
-      hidePreviusButton,
-      hideNextButton,
-      course,
-    } = this.state;
-    if (lessonData.length > 0) {
-      lessonBodyData = lessonData[currentPage];
-    }
-    return (
-      <>
-        <Helmet>
-          <meta
-            name="description"
-            content="Alexander Benavides| Cursos de programación web"
-            data-react-helmet="true"
-          />
-          <title>Módulo| {moduleTitle}</title>
-        </Helmet>
-        <div className="home-lessons">
-          <div className="home-lessons__back">
-            <Link
-              className="link-info"
-              to={{
-                pathname: `/courses`,
-              }}
-            >
-              <Button type="link" block>
-                Ir a todos los cursos
-              </Button>
-            </Link>
-          </div>
-          <div className="home-lessons__content">
-            <div className="home-lessons__content__modules">
-              <h3>Módulo: {moduleTitle}</h3>
-              <div className="grid-lessons">
-                <div className="grid-lessons__head">
-                  <h4>Temas</h4>
-                </div>
-                <ul className="grid-lessons__list">
-                  {lessonData.map((lesson, i) => {
-                    if (i === currentPage) {
-                      currentClass = "current__class";
-                    } else {
-                      currentClass = "";
-                    }
-                    return (
-                      <li
-                        className={`grid-lessons__list_options  ${currentClass}`}
-                        key={i}
-                      >
-                        {lesson.title}
-                      </li>
-                    );
-                  })}
-                </ul>
+  let currentClass = "";
+  let lessonBodyData = {
+    title: "",
+    content: "",
+  };
+  if (lessonData.length > 0) {
+    lessonBodyData = lessonData[currentPage];
+  }
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Alexander Benavides| Cursos de programación web"
+          data-react-helmet="true"
+        />
+        <title>Módulo| {moduleTitle}</title>
+      </Helmet>
+      <div className="home-lessons">
+        <div className="home-lessons__back">
+          <Link
+            className="link-info"
+            to={{
+              pathname: `/courses`,
+            }}
+          >
+            <Button type="link" block>
+              Ir a todos los cursos
+            </Button>
+          </Link>
+        </div>
+        <div className="home-lessons__content">
+          <div className="home-lessons__content__modules">
+            <h3>Módulo: {moduleTitle}</h3>
+            <div className="grid-lessons">
+              <div className="grid-lessons__head">
+                <h4>Temas</h4>
               </div>
-            </div>
-            <div className="home-lessons__content__lessons">
-              <div className="editorembed-container">
-                <div>{lessonBodyData.title}</div>
-                <div
-                  dangerouslySetInnerHTML={{ __html: lessonBodyData.content }}
-                ></div>
-              </div>
+              <ul className="grid-lessons__list">
+                {lessonData.map((lesson, i) => {
+                  if (i === currentPage) {
+                    currentClass = "current__class";
+                  } else {
+                    currentClass = "";
+                  }
+                  return (
+                    <li
+                      className={`grid-lessons__list_options  ${currentClass}`}
+                      key={i}
+                    >
+                      {lesson.title}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
-          <div className="home-lessons__butons">
-            <PreviousLesson
-              hidePreviusButton={hidePreviusButton}
-              handlePreviuosLesson={this.handlePreviuosLesson}
-            ></PreviousLesson>
-            <NextModule
-              course={course}
-              hideNextButton={hideNextButton}
-              handleNextLesson={this.handleNextLesson}
-            ></NextModule>
+          <div className="home-lessons__content__lessons">
+            <div className="editorembed-container">
+              <div>{lessonBodyData.title}</div>
+              <div
+                dangerouslySetInnerHTML={{ __html: lessonBodyData.content }}
+              ></div>
+            </div>
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="home-lessons__butons">
+          <PreviousLesson
+            hidePreviusButton={hidePreviusButton}
+            handlePreviuosLesson={handlePreviuosLesson}
+          ></PreviousLesson>
+          <NextModule
+            course={course}
+            hideNextButton={hideNextButton}
+            handleNextLesson={handleNextLesson}
+          ></NextModule>
+        </div>
+      </div>
+    </>
+  );
 }
 function PreviousLesson({ hidePreviusButton, handlePreviuosLesson }) {
   if (!hidePreviusButton) {
