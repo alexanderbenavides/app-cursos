045f773b39c1c8d740ea18cd64ef4508500507b0
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-05-02 21:53:34 -0500

    adding hooks Admin.js

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,79 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Popover, Modal } from "antd";
 import { MoreOutlined, DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { getInfoUserApi } from "../../api/auth";
 import AddUser from "../../components/Admin/Users//AddUser";
 
 import "./Admin.scss";
-class Admin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-      modalVisible: false
-    };
-  }
-  handleRemove = () => {
-    this.setState({ modalVisible: true });
+function Admin() {
+  const [visible, setVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleRemove = () => {
+    setModalVisible(true);
   };
-  handleEdit = () => {
-    this.setState({ modalVisible: true });
+  const handleEdit = () => {
+    setModalVisible(true);
   };
-  handleVisibleChange = visible => {
-    this.setState({ visible });
+  const handleVisibleChange = (visible) => {
+    setVisible(visible);
   };
-  handleCancel = () => {
-    this.setState({
-      modalVisible: false
-    });
+  const handleCancel = () => {
+    setModalVisible(false);
   };
-  render() {
-    const { name, lastname, role } = getInfoUserApi();
-    return (
-      <div className="admin__page">
-        <div className="admin__page__title">Información del usuario</div>
-        <div className="admin__page__grid">
-          <div>
-            <img
-              src="http://alexanderbenavides.herokuapp.com/img/perfil.1055441c.jpg"
-              alt="Not profile found"
-              className="admin__page__img"
-            />
-          </div>
-          <div className="admin__page__info">
-            <span>Nombre: {name}</span>
-            <span>Apellidos: {lastname}</span>
-            <span>Rol: {role}</span>
-          </div>
+
+  const { name, lastname, role } = getInfoUserApi();
+  return (
+    <div className="admin__page">
+      <div className="admin__page__title">Información del usuario</div>
+      <div className="admin__page__grid">
+        <div>
+          <img
+            src="http://alexanderbenavides.herokuapp.com/img/perfil.1055441c.jpg"
+            alt="Not profile found"
+            className="admin__page__img"
+          />
         </div>
-        <Popover
-          content={
-            <div className="admin__page__iconspopover">
-              <DeleteOutlined onClick={this.handleRemove} />
-              <EditOutlined onClick={this.handleEdit} />
-            </div>
-          }
-          title=""
-          trigger="click"
-          visible={this.state.visible}
-          onVisibleChange={this.handleVisibleChange}
-        >
-          <MoreOutlined className="admin__page__iconmore"></MoreOutlined>
-        </Popover>
-        <Modal
-          className="ant-modal-size"
-          title="Crear usuario"
-          visible={this.state.modalVisible}
-          onCancel={this.handleCancel}
-          maskClosable={false}
-          destroyOnClose={true}
-          footer={null}
-        >
-          <AddUser></AddUser>
-        </Modal>
+        <div className="admin__page__info">
+          <span>Nombre: {name}</span>
+          <span>Apellidos: {lastname}</span>
+          <span>Rol: {role}</span>
+        </div>
       </div>
-    );
-  }
+      <Popover
+        content={
+          <div className="admin__page__iconspopover">
+            <DeleteOutlined onClick={handleRemove} />
+            <EditOutlined onClick={handleEdit} />
+          </div>
+        }
+        title=""
+        trigger="click"
+        visible={visible}
+        onVisibleChange={handleVisibleChange}
+      >
+        <MoreOutlined className="admin__page__iconmore"></MoreOutlined>
+      </Popover>
+      <Modal
+        className="ant-modal-size"
+        title="Crear usuario"
+        visible={modalVisible}
+        onCancel={handleCancel}
+        maskClosable={false}
+        destroyOnClose={true}
+        footer={null}
+      >
+        <AddUser></AddUser>
+      </Modal>
+    </div>
+  );
 }
 
 export default Admin;
