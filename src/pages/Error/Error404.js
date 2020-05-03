56f2b131f7af6b5ff60c3c4d3e09c27979ede2ed
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-05-02 21:43:13 -0500

    using hooks errorPage

--- src/pages/Error/Error404.js
+++ src/pages/Error/Error404.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class Error404 extends React.Component {
-  render() {
-    return (
-      <div className="Error__404">
-        <h1>Página no encontrada</h1>
-      </div>
-    );
-  }
+function Error404() {
+  return (
+    <div className="Error__404">
+      <h1>Página no encontrada</h1>
+    </div>
+  );
 }
 
 export default Error404;
