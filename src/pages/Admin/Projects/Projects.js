734d257674e4cc215a50228a049281cc7e1c39c9
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-05-09 17:12:37 -0500

    add avatar

--- src/pages/Admin/Projects/Projects.js
+++ src/pages/Admin/Projects/Projects.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Modal, notification } from "antd";
+import { Modal, notification, Upload, message, Button } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+
 import {
   getProjectApi,
   addProjectApi,
   deleteProjectApi,
   updateProjectApi,
+  updateProjectAvatarApi,
 } from "../../../api/project";
 
 import { getAccessTokenApi } from "../../../api/auth";
@@ -12,6 +15,23 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import PorjectList from "../../../components/Admin/Projects";
 import AddProject from "../../../components/Admin/Projects/AddProject";
 import "../../../scss/_tables.scss";
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+}
 
 function Projects() {
   const [visibleModalAvatar, setVisibleModalAvatar] = useState(false);
@@ -21,6 +41,25 @@ function Projects() {
   const [titleModal, setTitleModal] = useState("");
   const [itemToEdit, setItemToEdit] = useState({});
   const [isHidden, setIsHidden] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [imgType, setImgType] = useState("");
+
+  const [project, setProject] = useState({});
+
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      getBase64(info.file.originFileObj, (imageUrl) => setImageUrl(imageUrl));
+      setLoading(false);
+      setImgType(info.file.type);
+    }
+  };
+
   useEffect(() => {
     getAllProjects();
   }, []);
@@ -161,9 +200,55 @@ function Projects() {
     }
   };
 
-  const handleupdateProjectAvatar = (openModal) => {
+  const handleupdateProjectAvatar = (openModal, item) => {
+    setProject(item);
     setVisibleModalAvatar(openModal);
   };
+
+  const handleSaveAvatar = () => {
+    const token = getAccessTokenApi();
+
+    const changeAvatarData = {
+      img: imageUrl,
+      project: project,
+      imgType: imgType,
+    };
+
+    if (changeAvatarData.img === "") {
+      notification["warning"]({
+        message: "Seleccionar una imagen.",
+      });
+      return;
+    }
+    updateProjectAvatarApi(token, changeAvatarData)
+      .then((response) => {
+        if (response?.status !== 200) {
+          setVisibleModalAvatar(true);
+          notification["warning"]({
+            message: "Hubo problemas editando el curso.",
+          });
+        } else {
+          getAllProjects();
+          setVisibleModalAvatar(false);
+          notification["success"]({
+            message: response.data.message,
+          });
+        }
+      })
+      .catch(() => {
+        setVisibleModalAvatar(true);
+        notification["error"]({
+          message: "No se pudo actualizar el proyecto.",
+        });
+      });
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div className="ant-upload-text">Subir imagen</div>
+    </div>
+  );
   return (
     <div className="table__container">
       <PlusCircleOutlined
@@ -200,7 +285,26 @@ function Projects() {
         footer={null}
         destroyOnClose={true}
       >
-        <div className="avatar-uploader__container"></div>
+        <div className="avatar-uploader__container">
+          <Upload
+            name="avatar"
+            listType="picture-card"
+            className="avatar-uploader"
+            showUploadList={false}
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+          >
+            {imageUrl ? (
+              <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
+          <Button type="primary" onClick={handleSaveAvatar}>
+            Guardar
+          </Button>
+        </div>
       </Modal>
     </div>
   );
