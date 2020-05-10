05f30b1cb1cf94dc8524fd9b0539dd67d6675a9a
Alexander Benavides <abcti.sistemas2018@gmail.com>
2020-05-09 19:17:20 -0500

    changing to react funcions

--- src/components/Admin/Projects/AddProject/AddProject.js
+++ src/components/Admin/Projects/AddProject/AddProject.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Input, Button, Switch, Spin, notification } from "antd";
 import { courseFormValidation } from "../../../../utils/courseFormValidation";
 
@@ -13,43 +13,44 @@ const layout = {
   },
 };
 
-class AddProject extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      itemToModify: this.props.itemToEdit,
-      isHidden: this.props.isHidden,
-      triggerProjectAction: this.props.triggerProjectAction,
-      formValidation: {
-        title: "",
-        content: "",
-        description: "",
-      },
-      formInvalid: "initial",
-    };
-  }
-  setValidationFormOnWriting = (item, property) => {
+function AddProject(props) {
+  const { itemToEdit, isHidden, triggerProjectAction, projectAction } = props;
+  const [formValidation, setFormValidation] = useState({
+    title: "",
+    content: "",
+    description: "",
+  });
+  const [itemToModify, setItemToModify] = useState({});
+
+  useEffect(() => {
+    setItemToModify(itemToEdit);
+  }, []);
+  const setValidationFormOnWriting = (item, property) => {
     const { error } = courseFormValidation(item, property);
-    this.setState((prevState) => ({
-      formValidation: {
-        ...prevState.formValidation,
-        [property]: error,
-      },
+    setFormValidation((prevState) => ({
+      ...prevState,
+      [property]: error,
     }));
   };
-  onChangeProperty = (item, property) => {
-    this.setState((prevState) => ({
-      itemToModify: {
-        ...prevState.itemToModify,
-        [property]: item,
-      },
+  const onChangeProperty = (item, property) => {
+    setFormValidation((prevState) => ({
+      ...prevState,
+      [property]: item,
+    }));
+
+    setItemToModify((prevState) => ({
+      ...prevState,
+      [property]: item,
     }));
   };
-  handleSubmitForm = (triggerProjectAction, itemToModify, editDeleteOrAdd) => {
+  const handleSubmitForm = (
+    triggerProjectAction,
+    itemToModify,
+    editDeleteOrAdd
+  ) => {
     if (editDeleteOrAdd === "deleteForm") {
       triggerProjectAction(itemToModify, editDeleteOrAdd);
     } else {
-      const { itemToModify } = this.state;
       const { inputs, error } = courseFormValidation(itemToModify, "fullForm");
       if (error === false) {
         triggerProjectAction(itemToModify, editDeleteOrAdd);
@@ -57,162 +58,152 @@ class AddProject extends React.Component {
         notification["warning"]({
           message: "Completar los campos correctamente.",
         });
-        this.setState((prevState) => ({
-          formInvalid: error,
-          formValidation: {
-            ...prevState.formValidation,
-            title: inputs.title,
-            content: inputs.content,
-            description: inputs.description,
-          },
+
+        setFormValidation((prevState) => ({
+          ...prevState,
+          title: inputs.title,
+          content: inputs.content,
+          description: inputs.description,
         }));
       }
     }
   };
-  render() {
-    const { Item } = Form;
-    const { projectAction, isHidden } = this.props;
-    let { itemToModify, formValidation, triggerProjectAction } = this.state;
-    const { TextArea } = Input;
-    return (
-      <Form {...layout} name="basic">
-        {projectAction !== "delete" ? (
-          <div>
-            <Item label="Título" name="title">
-              <div>
-                <Input
-                  className={formValidation.title}
-                  defaultValue={itemToModify.title}
-                  onChange={(e) =>
-                    this.onChangeProperty(e.target.value, "title")
-                  }
-                  onKeyUp={(e) =>
-                    this.setValidationFormOnWriting(e.target.value, "title")
-                  }
-                  onBlur={(e) =>
-                    this.setValidationFormOnWriting(e.target.value, "title")
-                  }
-                />
-              </div>
-            </Item>
-            <Item label="Descripción">
-              <TextArea
-                rows="5"
-                className={`customized-textarea ${formValidation.description}`}
-                defaultValue={itemToModify.description}
-                onChange={(e) =>
-                  this.onChangeProperty(e.target.value, "description")
-                }
+  const { Item } = Form;
+  const { TextArea } = Input;
+
+  console.log(itemToModify);
+
+  return (
+    <Form {...layout} name="basic">
+      {projectAction !== "delete" ? (
+        <div>
+          <Item label="Título" name="title">
+            <div>
+              <Input
+                className={formValidation.title}
+                value={itemToModify.title}
+                onChange={(e) => onChangeProperty(e.target.value, "title")}
                 onKeyUp={(e) =>
-                  this.setValidationFormOnWriting(e.target.value, "description")
+                  setValidationFormOnWriting(e.target.value, "title")
                 }
                 onBlur={(e) =>
-                  this.setValidationFormOnWriting(e.target.value, "description")
+                  setValidationFormOnWriting(e.target.value, "title")
                 }
               />
-            </Item>
-            <Item label="Contenido" className="editor__admin">
-              <div className={formValidation.content}>
-                <Editor
-                  value={itemToModify.content}
-                  init={{
-                    height: 400,
-                    menubar: true,
-                    plugins: [
-                      "advlist autolink lists link image charmap print preview anchor",
-                      "searchreplace visualblocks code fullscreen",
-                      "insertdatetime media table paste code help wordcount",
-                      "codesample",
-                      "hr",
-                    ],
-                    codesample_languages: [
-                      { text: "HTML/XML", value: "markup" },
-                      { text: "JavaScript", value: "javascript" },
-                      { text: "CSS", value: "css" },
-                      { text: "PHP", value: "php" },
-                      { text: "Ruby", value: "ruby" },
-                      { text: "Python", value: "python" },
-                      { text: "Java", value: "java" },
-                      { text: "C", value: "c" },
-                      { text: "C#", value: "csharp" },
-                      { text: "C++", value: "cpp" },
-                    ],
-                    toolbar:
-                      "undo redo | formatselect | bold italic backcolor | \
+            </div>
+          </Item>
+          <Item label="Descripción">
+            <TextArea
+              rows="5"
+              className={`customized-textarea ${formValidation.description}`}
+              value={itemToModify.description}
+              onChange={(e) => onChangeProperty(e.target.value, "description")}
+              onKeyUp={(e) =>
+                setValidationFormOnWriting(e.target.value, "description")
+              }
+              onBlur={(e) =>
+                setValidationFormOnWriting(e.target.value, "description")
+              }
+            />
+          </Item>
+          <Item label="Contenido" className="editor__admin">
+            <div className={formValidation.content}>
+              <Editor
+                value={itemToModify.content}
+                init={{
+                  height: 400,
+                  menubar: true,
+                  plugins: [
+                    "advlist autolink lists link image charmap print preview anchor",
+                    "searchreplace visualblocks code fullscreen",
+                    "insertdatetime media table paste code help wordcount",
+                    "codesample",
+                    "hr",
+                  ],
+                  codesample_languages: [
+                    { text: "HTML/XML", value: "markup" },
+                    { text: "JavaScript", value: "javascript" },
+                    { text: "CSS", value: "css" },
+                    { text: "PHP", value: "php" },
+                    { text: "Ruby", value: "ruby" },
+                    { text: "Python", value: "python" },
+                    { text: "Java", value: "java" },
+                    { text: "C", value: "c" },
+                    { text: "C#", value: "csharp" },
+                    { text: "C++", value: "cpp" },
+                  ],
+                  toolbar:
+                    "undo redo | formatselect | bold italic backcolor | \
                     alignleft aligncenter alignright alignjustify | \
                     bullist numlist outdent indent | removeformat | help | \
                     codesample | hr",
-                  }}
-                  onEditorChange={(e) => this.onChangeProperty(e, "content")}
-                  onBlur={(e) => {
-                    this.setValidationFormOnWriting(
-                      e.target.getContent(),
-                      "content"
-                    );
-                  }}
-                />
-              </div>
-            </Item>
-            <Item label="Publicado">
-              <Switch
-                defaultValue={itemToModify}
-                checkedChildren="Publicado"
-                unCheckedChildren="Suspendido"
-                checked={itemToModify.published}
-                onClick={(e) => this.onChangeProperty(e, "published")}
+                }}
+                onEditorChange={(e) => onChangeProperty(e, "content")}
+                onBlur={(e) => {
+                  setValidationFormOnWriting(e.target.getContent(), "content");
+                }}
               />
-            </Item>
-            <Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-              {projectAction === "add" ? (
-                <SpinButtonAddEdit
-                  isHidden={isHidden}
-                  textButton="Agregar"
-                  itemToModify={itemToModify}
-                  editDeleteOrAdd="addForm"
-                  triggerProjectAction={triggerProjectAction}
-                  buttonType="primary"
-                  isDanger={false}
-                  handleSubmitForm={this.handleSubmitForm}
-                ></SpinButtonAddEdit>
-              ) : (
-                <SpinButtonAddEdit
-                  isHidden={isHidden}
-                  textButton="Guardar"
-                  itemToModify={itemToModify}
-                  editDeleteOrAdd="editForm"
-                  triggerProjectAction={triggerProjectAction}
-                  buttonType="primary"
-                  isDanger={false}
-                  handleSubmitForm={this.handleSubmitForm}
-                ></SpinButtonAddEdit>
-              )}
-            </Item>
-          </div>
-        ) : (
-          <div>
-            <Item className="warning__message">
-              <label>
-                ¿ Está seguro que desea eliminar al curso {itemToModify.title}?
-                Esta acción también implica eliminar el contenido.
-              </label>
-            </Item>
-            <Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            </div>
+          </Item>
+          <Item label="Publicado">
+            <Switch
+              defaultValue={itemToModify}
+              checkedChildren="Publicado"
+              unCheckedChildren="Suspendido"
+              checked={itemToModify.published}
+              onClick={(e) => onChangeProperty(e, "published")}
+            />
+          </Item>
+          <Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            {projectAction === "add" ? (
               <SpinButtonAddEdit
                 isHidden={isHidden}
-                textButton="Eliminar"
+                textButton="Agregar"
                 itemToModify={itemToModify}
-                editDeleteOrAdd="deleteForm"
+                editDeleteOrAdd="addForm"
                 triggerProjectAction={triggerProjectAction}
                 buttonType="primary"
-                isDanger={true}
-                handleSubmitForm={this.handleSubmitForm}
+                isDanger={false}
+                handleSubmitForm={handleSubmitForm}
               ></SpinButtonAddEdit>
-            </Item>
-          </div>
-        )}
-      </Form>
-    );
-  }
+            ) : (
+              <SpinButtonAddEdit
+                isHidden={isHidden}
+                textButton="Guardar"
+                itemToModify={itemToModify}
+                editDeleteOrAdd="editForm"
+                triggerProjectAction={triggerProjectAction}
+                buttonType="primary"
+                isDanger={false}
+                handleSubmitForm={handleSubmitForm}
+              ></SpinButtonAddEdit>
+            )}
+          </Item>
+        </div>
+      ) : (
+        <div>
+          <Item className="warning__message">
+            <label>
+              ¿ Está seguro que desea eliminar al curso {itemToModify.title}?
+              Esta acción también implica eliminar el contenido.
+            </label>
+          </Item>
+          <Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <SpinButtonAddEdit
+              isHidden={isHidden}
+              textButton="Eliminar"
+              itemToModify={itemToModify}
+              editDeleteOrAdd="deleteForm"
+              triggerProjectAction={triggerProjectAction}
+              buttonType="primary"
+              isDanger={true}
+              handleSubmitForm={handleSubmitForm}
+            ></SpinButtonAddEdit>
+          </Item>
+        </div>
+      )}
+    </Form>
+  );
 }
 
 function SpinButtonAddEdit({
